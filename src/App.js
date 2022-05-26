138500bd03365ad0cf59d1ea884bf637bb68db80
Sam Shearer <85294886+sshearer101@users.noreply.github.com>
2022-05-26 13:59:58 -0600

    checked

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react"
 
 function App() {
+
+
+  const [checkedOne, setCheckedOne] = useState(false);
+  const [checkedTwo, setCheckedTwo] = useState(false);
+
+  const handleChangeOne = () => {
+    setCheckedOne(!checkedOne);
+  };
+
+  const handleChangeTwo = () => {
+    setCheckedTwo(!checkedTwo);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1>Do you like cats? {checkedOne ? "Yes" : "No"}</h1>
+
+      <br></br>
+      Yes
+      <input
+        type="checkbox"
+        label="Value 1"
+        value={checkedOne}
+        onChange={handleChangeOne}
+      />
+      <br></br>
+      <h1>Do you like dogs? {checkedTwo ? "Yes" : "No"}</h1>
+      <br></br>
+      Yes
+      <input
+        type="checkbox"
+        label="Value 2"
+        value={checkedTwo}
+        onChange={handleChangeTwo}
+      />
     </div>
   );
+
+
+
+
+//   const [likesCats, setLikesCats] = useState(false)
+//   const [likesDogs, setLikesDogs] = useState(false)
+
+//   function handleChange(e){
+//     if (e.target.type === "checkbox"){
+//       setLikesCats(e.target.name : e.target.checked)
+//       setLikesDogs(e.target.name)
+//     }
+//     setLikesCats(e.target.name : e.target.checked)
+//     setLikesDogs(e.target.name)
+//  }
+
+
+
+//   return (
+//     <div className="App">
+//       <h1>Do you like cats? {likesCats ? "Yes" : "No"}</h1>
+//       Yes
+//       <input
+//         type="checkbox"
+//         name='likesCats'
+//         onChange={handleChange}
+//       >
+//       </input>
+    
+    
+//     <br></br>
+
+//     <h1>Do you like dogs? {likesDogs ? "Yes" : "No"}</h1>
+//       Yes
+//       <input
+//         type="checkbox"
+//         name="likesDogs"
+//         onChange={handleChange}
+//       >
+//       </input>
+ 
+    
+//     </div>
+//   );
 }
 
 export default App;
